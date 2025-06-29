7b948a5e92361fa5db6c83f873237604e03f2406
Arthur Miller <arthur.miller@live.com>
2025-06-29 20:07:16 +0200

    Initial commit

---
+++ ansi-common-lisp-draft.el
@@ -0,0 +1,32 @@
+;;; ansi-common-lisp.el --- A dummy file to install ANSI doc in info manual  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Arthur Miller
+
+;; Author: Arthur Miller <arthur.miller@live.com>
+;; Keywords: 
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is draft of ANSI Common Lisp standard in info format, as found on John
+;; Wiegleys ready-lisp: https://github.com/jwiegley/ready-lisp
+
+;; This dummy elisp file is needed to trick package.el to install info files
+
+;;; Code:
+
+(provide 'ansi-common-lisp)
+;;; ansi-common-lisp.el ends here
